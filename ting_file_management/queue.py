39aaefb691c268b524b34ff93988f6eccfd0ed82
Guilherme Oliveira <guilherme.moliveira@hotmail.com>
2023-02-03 16:05:41 -0300

    feat: class Queue Implemenae uma fila para armazenar os arquivos que serão lidos do tipo FIFO

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,18 +1,25 @@
 from ting_file_management.abstract_queue import AbstractQueue
-#
+
 
 class Queue(AbstractQueue):
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        self.queue = list()
+        self.length = 0
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return self.length
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        self.queue.append(value)
+        self.length += 1
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        if len(self.queue) > 0:
+            self.length -= 1
+            return self.queue.pop(0)
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        if 0 <= index <= len(self.queue):
+            return self.queue[index]
+        else:
+            raise IndexError
