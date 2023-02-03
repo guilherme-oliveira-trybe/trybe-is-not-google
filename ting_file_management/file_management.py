8d5715144eb6e58901733a4c91670805b6cb9608
Guilherme Oliveira <guilherme.moliveira@hotmail.com>
2023-02-03 16:43:09 -0300

    feat: def txt_importer capaz de importar notícias  a partir de um arquivo TXT

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,13 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    try:
+        if path_file.endswith(".txt"):
+            with open(path_file) as file:
+                read_file = file.read().splitlines()
+                return read_file
+
+        return sys.stderr.write("Formato inválido")
+    except FileNotFoundError:
+        return sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
