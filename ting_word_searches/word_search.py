8db29e0159c21e6bb77943526a09fc7e277e44a3
Guilherme Oliveira <guilherme.moliveira@hotmail.com>
2023-02-06 15:54:06 -0300

    feat: def exists_word verifica a existência de uma palavra  em todos os arquivos processados

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,17 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    text_exists_word = []
+    for path in instance.queue:
+        ocorrencias = []
+        for i, linha in enumerate(path["linhas_do_arquivo"]):
+            if word in linha.lower():
+                ocorrencias.append({"linha": i + 1})
+        if len(ocorrencias) > 0:
+            text_exists_word.append({
+                "palavra": word,
+                "arquivo": path["nome_do_arquivo"],
+                "ocorrencias": ocorrencias
+            })
+    return text_exists_word
 
 
 def search_by_word(word, instance):
