1719b9ceba04a0c1194a3e6d6fb0f6f083597fd9
Guilherme Oliveira <guilherme.moliveira@hotmail.com>
2023-02-07 11:32:47 -0300

    refactor(word_search): def exists_word limpando o código

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,16 +1,9 @@
 def exists_word(word, instance):
-    text_exists_word = []
-    for path in instance.queue:
-        ocorrencias = []
-        for i, linha in enumerate(path["linhas_do_arquivo"]):
-            if word in linha.lower():
-                ocorrencias.append({"linha": i + 1})
-        if len(ocorrencias) > 0:
-            text_exists_word.append({
-                "palavra": word,
-                "arquivo": path["nome_do_arquivo"],
-                "ocorrencias": ocorrencias
-            })
+    text_exists_word = search_by_word(word, instance)
+    if len(text_exists_word) > 0:
+        for text in text_exists_word:
+            for t in text["ocorrencias"]:
+                t.pop("conteudo")
     return text_exists_word
 
 
